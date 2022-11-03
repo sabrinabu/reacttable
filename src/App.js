98c8b93eb67d7faaccf2677d9d88a80eb3831c6d
sabrinabu <sabrinakhaled13@gmail.com>
2022-11-03 21:01:31 +0100

    second

--- src/App.js
+++ src/App.js
@@ -3,8 +3,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <h1>hello table</h1>
+    <div className="app-container">
+   <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Address</th>
+        <th>Phone Number</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Sabrina Rahman</td>
+        <td>Berlin</td>
+        <td>02583</td>
+        <td>abc@gmail.com</td>
+      </tr>
+    </tbody>
+   </table>
     </div>
   );
 }
